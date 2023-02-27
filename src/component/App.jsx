a0a63fdb1e91011a3c128e41f3159fae8fbc39b0
Richard <110678590+Amehoboni@users.noreply.github.com>
2023-02-27 12:00:30 +0100

    Mapped components

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+// function createNotes(note) {
+//   return <Note key={note.id} title={note.title} content={note.content} />
+// }
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => (
+        <Note key={note.id} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
